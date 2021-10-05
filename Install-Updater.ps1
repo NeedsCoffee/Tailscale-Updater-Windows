6058dfb3802aff676e6c79e552956834994c8229
NeedsCoffee <nick@huggingservers.uk>
2021-10-05 14:36:14 +0100

    tidy output of installer

--- Install-Updater.ps1
+++ Install-Updater.ps1
@@ -24,9 +24,9 @@ Try {
     $target = Get-Item -LiteralPath $path
     Write-Host "Installing into $target"
     Get-ChildItem "Tailscale-Updater-Windows.ps1" | Copy-Item -Destination $target -Force
-    $action = New-ScheduledTaskAction -Execute 'powershell.exe' -WorkingDirectory $target.FullName -Argument ('-NoProfile -NoLogo -WindowStyle Hidden -ExecutionPolicy Bypass -File Tailscale-Updater-Windows.ps1'+(if($unstable){' -Track unstable'}))
+    $action = New-ScheduledTaskAction -Execute 'powershell.exe' -WorkingDirectory $target.FullName -Argument ('-NoProfile -NoLogo -WindowStyle Hidden -ExecutionPolicy Bypass -File Tailscale-Updater-Windows.ps1'+$(if($unstable){' -Track unstable'}))
     $trigger = New-ScheduledTaskTrigger -Daily -At 12:00
-    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Tailscale Updater" -Description "Daily Tailscale update" -User "NT AUTHORITY\SYSTEM"
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Tailscale Updater" -Description "Daily Tailscale update" -User "NT AUTHORITY\SYSTEM" | Out-Null
     Write-Host "Successfully installed scheduled task"
 } Catch {
     $_ | Write-Error
