d1d1d48f3287209455eec9939c58dfb47a56f134
NeedsCoffee <nick@huggingservers.uk>
2021-10-05 14:29:30 +0100

    update installer to copy files and handle unstable

--- Install-Updater.ps1
+++ Install-Updater.ps1
@@ -1,3 +1,33 @@
-$action = New-ScheduledTaskAction -Execute 'powershell.exe' -WorkingDirectory (Get-Location) -Argument '-NoProfile -NoLogo -WindowStyle Hidden -ExecutionPolicy Bypass -File Tailscale-Updater-Windows.ps1'
-$trigger = New-ScheduledTaskTrigger -Daily -At 12:00
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Tailscale Updater" -Description "Daily Tailscale update" -User "NT AUTHORITY\SYSTEM"
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [switch]
+    $unstable = $false,
+    [Parameter()]
+    [string]
+    $path = "$env:ALLUSERSPROFILE\Tailscale-updater"
+)
+
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "Not running as Admin. Nothing done."
+    break
+} else {
+    Write-Host "Admin rights available"
+}
+
+if(-not (Test-Path -LiteralPath $path)){
+    Write-Host "Creating directory $path"
+    $target = New-Item -Path $path -ItemType Directory
+}
+
+Try {
+    $target = Get-Item -LiteralPath $path
+    Write-Host "Installing into $target"
+    Get-ChildItem "Tailscale-Updater-Windows.ps1" | Copy-Item -Destination $target -Force
+    $action = New-ScheduledTaskAction -Execute 'powershell.exe' -WorkingDirectory $target.FullName -Argument ('-NoProfile -NoLogo -WindowStyle Hidden -ExecutionPolicy Bypass -File Tailscale-Updater-Windows.ps1'+(if($unstable){' -Track unstable'}))
+    $trigger = New-ScheduledTaskTrigger -Daily -At 12:00
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Tailscale Updater" -Description "Daily Tailscale update" -User "NT AUTHORITY\SYSTEM"
+    Write-Host "Successfully installed scheduled task"
+} Catch {
+    $_ | Write-Error
+}
